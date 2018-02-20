d0d8f854858821d2f638a2132a6d08c0c3df045c
Tyson Hood <TysonHood@gmail.com>
2018-02-20 09:55:04 -0800

    Get boxes to change color. Work on individual selection.

--- main.js
+++ main.js
@@ -2,15 +2,24 @@
 var boxes = document.getElementsByClassName("box");
 console.log(boxes.length);
 
+var randoColor;
+
 // recognize when a box is clicked
 for (var i = 0; i < boxes.length; i++) {
+// add color to each box individually
+	randoColor = 	'rgb(' + 
+									(Math.floor(Math.random() * 256)) + ',' +
+									(Math.floor(Math.random() * 256)) + ',' +
+									(Math.floor(Math.random() * 256)) + ')'
+
 	console.log("Hello!")
+	console.log(i)
+	console.log(randoColor);
+
+	boxes[i].style.backgroundColor = randoColor;
 
 	boxes[i].addEventListener("click", function() {
-		console.log("You clicked me!")
+		console.log("Box No. " + i)
 	})
 }
 
-// add color to each box individually
-var randoColor = (Math.floor(Math.random() * 256))
-console.log(randoColor);
