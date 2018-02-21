b5a8bb29f2e6e776753e2f56719965229146c440
Tyson Hood <TysonHood@gmail.com>
2018-02-21 13:46:42 -0800

    Complete main functionality of game to select random correct color.

--- main.js
+++ main.js
@@ -19,11 +19,26 @@ for (var i = 0; i < boxes.length; i++) {
 	console.log(randoColor);
 
 	boxes[i].style.backgroundColor = randoColor;
+	theColor = boxes[(Math.floor(Math.random() * 5))].style.backgroundColor
 
 	box = document.getElementById("box" + i.toString())
 	box.addEventListener("click", function() {
-		this.style.backgroundColor = 'red';
+		console.log(this.style.backgroundColor)
 		// console.log(i)
 		console.log(this.id)
+
+		if (this.style.backgroundColor ==  theColor) {
+			console.log("Correct!")
+
+			// loop through boxes
+			for (var i = 0; i < boxes.length; i++) {
+				boxes[i].style.backgroundColor = theColor;
+			}
+		} else {
+			console.log("Nope!")
+			this.style.backgroundColor = 'red';
+		}
+
+
 	})
 }
